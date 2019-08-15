5e19eb6a4f9b724acacbc1ccf44b8de1d819e089
Tia Lendor <TiaLendon@pursuit.org>
2019-08-15 19:59:43 -0400

    doneish

--- SongsTableViewSearchBar/Controllers/ViewController.swift
+++ SongsTableViewSearchBar/Controllers/ViewController.swift
@@ -9,15 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
- let songs = Song.loveSongs
     
+//    MARK -- Outlets
+    @IBOutlet weak var searchBarOutlet: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    
+//    MARK -- Data Source
+    let songs = Song.loveSongs
+    var songSearchString: String? = nil {
+        didSet{
+            print(songSearchString)
+            self.tableView.reloadData()
+        }
+    }
+    
+    var userRequestedSearch: [Song] {
+        guard let searchedString = songSearchString else{
+            return songs
+        }
+        guard searchedString != "" else{
+            return songs
+        }
+        
+        if let scopeTitles = searchBarOutlet.scopeButtonTitles {
+            let currentScopeIndex = searchBarOutlet.selectedScopeButtonIndex
+            switch scopeTitles[currentScopeIndex] {
+            case "song title":
+//                filtered all the name letters in the database to lowercased to be equal the letters in SongSearchString
+                return songs.filter{$0.name.lowercased().contains(searchedString.lowercased())}
+            case "artist name":
+                return songs.filter{$0.artist.lowercased().contains(searchedString.lowercased())}
+            default:
+                return songs.filter{$0.name.contains(searchedString.lowercased()) || $0.artist.contains(searchedString.lowercased())}
+            }
+        }
+        return songs
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableView.delegate = self
         tableView.dataSource = self
+        searchBarOutlet.delegate = self
     }
     
 
@@ -52,17 +88,21 @@ class ViewController: UIViewController {
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return songs.count
+        return userRequestedSearch.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let song = songs[indexPath.row]
+        let song = userRequestedSearch[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
             cell.textLabel?.text = song.name
             cell.detailTextLabel?.text = song.artist
         
         return cell
     }
+}
 
-
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        songSearchString = searchBar.text
+    }
 }
